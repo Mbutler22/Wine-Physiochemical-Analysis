a45fe49bcf77a991e48d36f905b7cd83996a921f
Dasha <84484371+DasaSimo@users.noreply.github.com>
2021-11-06 10:33:00 -0400

    adding code in red wines, fixing html

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, redirect,jsonify
+from flask import Flask, render_template, redirect,jsonify,request
+from joblib import load
 import socket
 import os
 
@@ -22,6 +23,36 @@ def ml():
    
    return render_template("machine_learning.html")
 
+@app.route('/white_data', methods=['POST'])
+def white_data():
+    print('white_data')
+    outcome="something"
+    return render_template("machine_learning.html", routcome=outcome)
+
+@app.route('/red_data', methods=['POST'])
+def red_data():
+    print('red_data')
+    X=[]
+    X.append(request.form['rvolacidity'])
+    #print('before sulfure')
+    X.append(request.form['rTotalSulfur'])
+    X.append(request.form['rDensity'])
+    X.append(request.form['rSulfates'])
+    X.append(request.form['rAlcohol'])
+    print("before XX")
+    XX=[X]
+    print('before load')
+    classifier = load('Red_Wine_Quality.joblib')
+    print("before predict")
+    result=classifier.predict(XX)
+
+    if (result==0):
+        outcome="Hmm... there are better wines"
+    else:
+        outcome="Good Choice!"
+    print(f"Sulfur:{request.form['rTotalSulfur']}<br>{outcome}")
+    return render_template("machine_learning.html", routcome=outcome)
+    
 
 @app.route("/team.html")
 def team():
