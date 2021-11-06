45acbdc5b05b0d1a30c7da7001736e6bcad4bc8a
Dasha <84484371+DasaSimo@users.noreply.github.com>
2021-11-06 13:42:52 -0400

    adding white wines properties into html

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ app = Flask(__name__)
 
 
 
+
+
 @app.route("/")
 @app.route("/index.html")
 def index():
@@ -20,38 +22,47 @@ def visitations():
 
 @app.route("/machine_learning.html")
 def ml():
-   
-   return render_template("machine_learning.html")
+   global rvolacidity, rTotalSulfur, rDensity, rSulfates, rAlcohol
+   global routcome 
+   routcome = ""
+   rvolacidity = 0.12
+   rTotalSulfur = 6.0
+   rDensity = 0.990
+   rSulfates = 0.33
+   rAlcohol = 8.4
+   return render_template("machine_learning.html", routcome=routcome, rvolacidity=rvolacidity,\
+                            rTotalSulfur=rTotalSulfur , rDensity= rDensity, rSulfates=rSulfates, rAlcohol=rAlcohol)
 
 @app.route('/white_data', methods=['POST'])
 def white_data():
     print('white_data')
-    outcome="something"
-    return render_template("machine_learning.html", routcome=outcome)
+    return render_template("machine_learning.html", routcome=routcome, rvolacidity=rvolacidity,\
+                            rTotalSulfur=rTotalSulfur , rDensity= rDensity, rSulfates=rSulfates, rAlcohol=rAlcohol)
 
 @app.route('/red_data', methods=['POST'])
 def red_data():
-    print('red_data')
     X=[]
-    X.append(request.form['rvolacidity'])
-    #print('before sulfure')
-    X.append(request.form['rTotalSulfur'])
-    X.append(request.form['rDensity'])
-    X.append(request.form['rSulfates'])
-    X.append(request.form['rAlcohol'])
-    print("before XX")
+    rvolacidity= request.form['rvolacidity']
+    X.append(rvolacidity)
+    rTotalSulfur = request.form['rTotalSulfur']
+    X.append(rTotalSulfur)
+    rDensity= request.form['rDensity']
+    X.append(rDensity)
+    rSulfates = request.form['rSulfates']
+    X.append(rSulfates)
+    rAlcohol = request.form['rAlcohol']
+    X.append(rAlcohol)
     XX=[X]
-    print('before load')
     classifier = load('Red_Wine_Quality.joblib')
-    print("before predict")
     result=classifier.predict(XX)
 
     if (result==0):
-        outcome="Hmm... there are better wines"
+        routcome="Hmm... there are better wines"
     else:
-        outcome="Good Choice!"
-    print(f"Sulfur:{request.form['rTotalSulfur']}<br>{outcome}")
-    return render_template("machine_learning.html", routcome=outcome)
+        routcome="Good Choice!"
+    return render_template("machine_learning.html", routcome=routcome, rvolacidity=rvolacidity,\
+                            rTotalSulfur=rTotalSulfur , rDensity= rDensity, rSulfates=rSulfates, rAlcohol=rAlcohol)
+
     
 
 @app.route("/team.html")
