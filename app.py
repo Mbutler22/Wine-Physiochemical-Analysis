78729ddd958b4dd994f33d2dc1ce67a1ba568fac
Dasha <84484371+DasaSimo@users.noreply.github.com>
2021-11-06 14:26:55 -0400

    adding functionality for second model

--- app.py
+++ app.py
@@ -20,27 +20,61 @@ def visitations():
    
    return render_template("visualizations.html")
 
+routcome = ""
+rvolacidity = 0.12
+rTotalSulfur = 6.0
+rDensity = 0.990
+rSulfates = 0.33
+rAlcohol = 8.4
+woutcome =""
+wAcidity = 0.08
+wFreeSulfur = 2.00
+wDensity = 0.987
+wAlcohol = 8.0
+
 @app.route("/machine_learning.html")
 def ml():
    global rvolacidity, rTotalSulfur, rDensity, rSulfates, rAlcohol
-   global routcome 
-   routcome = ""
-   rvolacidity = 0.12
-   rTotalSulfur = 6.0
-   rDensity = 0.990
-   rSulfates = 0.33
-   rAlcohol = 8.4
+   global wAcidity, wFreeSulfur, wDensity, wAlcohol
+   global routcome, woutcome 
+
    return render_template("machine_learning.html", routcome=routcome, rvolacidity=rvolacidity,\
-                            rTotalSulfur=rTotalSulfur , rDensity= rDensity, rSulfates=rSulfates, rAlcohol=rAlcohol)
+                            rTotalSulfur=rTotalSulfur , rDensity= rDensity, rSulfates=rSulfates, rAlcohol=rAlcohol,\
+                            woutcome = woutcome, wAcidity=wAcidity, wFreeSulfur=wFreeSulfur,wDensity = wDensity,\
+                            wAlcohol =wAlcohol)
+
 
 @app.route('/white_data', methods=['POST'])
 def white_data():
-    print('white_data')
-    return render_template("machine_learning.html", routcome=routcome, rvolacidity=rvolacidity,\
-                            rTotalSulfur=rTotalSulfur , rDensity= rDensity, rSulfates=rSulfates, rAlcohol=rAlcohol)
+    global rvolacidity, rTotalSulfur, rDensity, rSulfates, rAlcohol
+    global wAcidity, wFreeSulfur, wDensity, wAlcohol
+    global routcome, woutcome 
+    X=[]
+    wAcidity= request.form['wAcidity']
+    X.append(wAcidity)
+    wFreeSulfur = request.form['wFreeSulfur']
+    X.append(wFreeSulfur)
+    wDensity= request.form['wDensity']
+    X.append(wDensity)
+    wAlcohol = request.form['wAlcohol']
+    X.append(wAlcohol)
+    XX=[X]
+    classifier = load('white_wine_model.joblib')
+    result=classifier.predict(XX)
+
+    if (result==0):
+        woutcome="Hmm... there are better wines"
+    else:
+        woutcome="Good Choice!"
+    return redirect("/machine_learning.html")
+
+
 
 @app.route('/red_data', methods=['POST'])
 def red_data():
+    global rvolacidity, rTotalSulfur, rDensity, rSulfates, rAlcohol
+    global wAcidity, wFreeSulfur, wDensity, wAlcohol
+    global routcome, woutcome 
     X=[]
     rvolacidity= request.form['rvolacidity']
     X.append(rvolacidity)
@@ -60,8 +94,7 @@ def red_data():
         routcome="Hmm... there are better wines"
     else:
         routcome="Good Choice!"
-    return render_template("machine_learning.html", routcome=routcome, rvolacidity=rvolacidity,\
-                            rTotalSulfur=rTotalSulfur , rDensity= rDensity, rSulfates=rSulfates, rAlcohol=rAlcohol)
+    return redirect("/machine_learning.html")
 
     
 
