2fadd430d7ef9ffc1906048b8388cbd42a316820
Dasha <84484371+DasaSimo@users.noreply.github.com>
2021-11-07 22:02:22 -0500

    adding functionality for visualization with tableau

--- app.py
+++ app.py
@@ -6,19 +6,21 @@ import os
 app = Flask(__name__)
 
 
+class WWines: 
+    def __init__(self):
+        self.outcome=""
+        self.country=1
+        self.price=1
+        self.variety=1
 
+    def getData(self, request):
+        self.country = int(request.form['country'])
+        self.price = int(request.form['price'])
+        self.variety = int(request.form['variety'])
+        print(self.country)
+        print(self.price)
+        print(self.variety)
 
-
-@app.route("/")
-@app.route("/index.html")
-def index():
-    return render_template("index.html")
-
-
-@app.route("/visualizations.html")
-def visitations():
-   
-   return render_template("visualizations.html")
 
 routcome = ""
 rvolacidity = 0.12
@@ -32,23 +34,39 @@ wFreeSulfur = 2.00
 wDensity = 0.987
 wAlcohol = 8.0
 
+gwine = WWines()
+
+@app.route("/")
+@app.route("/index.html")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/visualizations.html")
+def visualization():
+   
+   return render_template("visualizations.html")
+
+# will do: change to class 
+
 @app.route("/machine_learning.html")
 def ml():
    global rvolacidity, rTotalSulfur, rDensity, rSulfates, rAlcohol
    global wAcidity, wFreeSulfur, wDensity, wAlcohol
-   global routcome, woutcome 
+   global gwine 
+   global routcome, woutcome  
 
    return render_template("machine_learning.html", routcome=routcome, rvolacidity=rvolacidity,\
                             rTotalSulfur=rTotalSulfur , rDensity= rDensity, rSulfates=rSulfates, rAlcohol=rAlcohol,\
                             woutcome = woutcome, wAcidity=wAcidity, wFreeSulfur=wFreeSulfur,wDensity = wDensity,\
-                            wAlcohol =wAlcohol)
+                            wAlcohol =wAlcohol,\
+                            gwine=gwine)
 
 
 @app.route('/white_data', methods=['POST'])
 def white_data():
-    global rvolacidity, rTotalSulfur, rDensity, rSulfates, rAlcohol
-    global wAcidity, wFreeSulfur, wDensity, wAlcohol
-    global routcome, woutcome 
+    global wAcidity, wFreeSulfur, wDensity, wAlcohol  
+    global woutcome 
     X=[]
     wAcidity= request.form['wAcidity']
     X.append(wAcidity)
@@ -73,8 +91,7 @@ def white_data():
 @app.route('/red_data', methods=['POST'])
 def red_data():
     global rvolacidity, rTotalSulfur, rDensity, rSulfates, rAlcohol
-    global wAcidity, wFreeSulfur, wDensity, wAlcohol
-    global routcome, woutcome 
+    global routcome  
     X=[]
     rvolacidity= request.form['rvolacidity']
     X.append(rvolacidity)
@@ -96,15 +113,33 @@ def red_data():
         routcome="Good Choice!"
     return redirect("/machine_learning.html")
 
-    
+@app.route('/world_data', methods=['POST'])
+def world_data():
+    global gwine
+    gwine.getData(request)
+    testdata = [0] * 45
+    testdata[gwine.country-1]= 1
+    testdata[gwine.price+36]= 1
+    testdata[gwine.variety+6]= 1
+    XX=[testdata]
+    classifier = load('World_Wine_Quality.joblib')
+    result=classifier.predict(XX)
+
+    if (result==0):
+        gwine.outcome="Hmm... there are better wines"
+    else:
+        gwine.outcome="Good Choice!"
+    return redirect("/machine_learning.html")    
 
 @app.route("/team.html")
 def team():
     return render_template("team.html")
 
 
-
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
 
 
